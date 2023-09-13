86b9d439b551d60d65939ce053239aac1ae8d1dd
Ilia <ivanovantik@gmail.com>
2023-09-13 19:14:38 +0300

    add PUT and DELETE methods

--- api/urls.py
+++ api/urls.py
@@ -6,11 +6,14 @@ from . import views
 # определяем namespace для, например, < a href 'app:method_name'...>
 app_name = 'api'
 urlpatterns = [
-    
     # получение всех постов
     path('posts/', views.posts, name='posts'),
     # получение поста
     path('posts/<int:post_id>/', views.get_post, name='get_post'),
     # добавление поста
     path('add/', views.add_post, name='add_post'),
+    # редактирование поста
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    # удаление поста
+    path('delete/<int:post_id>/', views.delete_post, name="delete_post")
     ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from blogs_app.models import BlogPost
 from .serializers import BlogPostSerializer
 from django.shortcuts import get_object_or_404
@@ -24,6 +25,47 @@ def add_post(request):
         serializer.save()
     # вот здесь else будет
     return Response(serializer.data)
+
+@api_view(['GET','PUT'])
+def edit_post(request, post_id):
+    """Изменение записи по post_id. При GET-запросе выводит текущую запись\
+        для просмотра. При PUT-запросе формирует измененную запись из\
+        переданных в request данных и текущих данных (если в request заполнены не все поля)"""
+    # пытаемся получить запись по post_id
+    try:
+        post = BlogPost.objects.get(id=post_id)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    # при GET возвращаем текущую запись для просмотра
+    if(request.method == 'GET'):
+        return Response(BlogPostSerializer(post).data)
+    # при PUT берем данные из request и дополняем их значениями текущей записи (если не хватает)
+    elif(request.method == 'PUT'):
+        new_data = request.data.copy()
+        if(not new_data.get('title')):
+            new_data['title'] = post.title
+        if(not new_data.get('text')):
+            new_data['text'] = post.text
+        if(not new_data.get('owner')):
+            new_data['owner'] = post.owner.id
+        # не забыть передать instance, чтобы изменялась текущая запись, а не создавалась новая
+        serializer = BlogPostSerializer(instance=post, data=new_data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_post(request, post_id):
+    """Удаление записи по post_id. Only DELETE-method"""
+    try:
+        post = BlogPost.objects.get(id=post_id)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    post.delete()
+    return Response({"info": "deleted succesfully"}, status=status.HTTP_204_NO_CONTENT)
+
 
 
 
